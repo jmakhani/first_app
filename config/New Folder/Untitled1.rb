730a8ee897f89e8cfc3654c9988ad0cfb2cb92ff
Jeen <jmakhani@uncc.edu>
2017-02-24 16:00:32 +0000

    test

---
+++ config/New Folder/Untitled1.rb
@@ -0,0 +1,14 @@
+class Class
+  def attr_accessor_with_history(attr_name)
+    attr_name = attr_name.to_s # make sure it's a string
+    attr_reader attr_name # create the attribute's getter
+    attr_reader attr_name+"_history" # create bar_history getter
+    class_eval %Q{
+      def #{attr_name}= (new_val)
+        @#{attr_name}_history = [] if @#{attr_name}_history.nil?        
+        @#{attr_name}_history.push(@#{attr_name})
+        @#{attr_name} = new_val
+      end
+    }
+  end
+end
